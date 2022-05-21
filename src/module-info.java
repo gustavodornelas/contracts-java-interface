92084b08acd3e51fb5348a6252f924883727f201
“gustavodornelas” <dornelas.gustavoptc@gmail.com>
2022-05-21 17:04:28 -0300

    Adding database acess

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module javaFX1 {
 	requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.controls;
+	requires java.sql;
         
 	opens application to javafx.graphics, javafx.fxml, javafx.controls;
 	opens gui to javafx.fxml;
