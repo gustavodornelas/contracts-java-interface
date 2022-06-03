1e6dc08ba2299b3e08f1e006560a70a4f3539abd
“gustavodornelas” <dornelas.gustavoptc@gmail.com>
2022-06-03 19:44:19 -0300

    Validation exception

---
+++ src/model/exceptions/ValidationException.java
@@ -0,0 +1,23 @@
+package model.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationException extends RuntimeException{
+
+	private Map<String, String> errors = new HashMap<>();
+	
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String msg) {
+		super(msg);
+	}
+	
+	public Map<String, String> getErrors(){
+		return errors;
+	}
+	
+	public void addErrors(String fieldName, String errorMessage) {
+		errors.put(fieldName, errorMessage);
+	}
+}
